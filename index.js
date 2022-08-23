7d24afd858cf9e8eff5c14d55f33ec3e8e6e1095
Nicolas Telep <ndtelep@gmail.com>
2022-08-23 11:16:55 -0600

    added edward's work to fully populate feed

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
-// function addMemes (){
-//     let seeMemes = document.getElementById("meme-list");
-//     seeMemes.innerText = "this is where grumpy cat should be"
-// }
-
-// addMemes ()
-
-// let memeFeed = document.getElementById("meme-list");
-
-// fetch("https://api.imgflip.com/get_memes")
-// .then(response => response.json())
-// .then(memeInfo => {
-//     navBar(memeInfo)
-// })
-
-// function navBar(memeInfo){
-//     let memeInfo = .append(navItem)
-// }
-
 let memeFeed = document.getElementById("meme-list")
-let navItem = document.createElement('img')
-
+let navItem = document.createElement('h5')
+let i = 0
 fetch("https://api.imgflip.com/get_memes")
 .then(response => response.json())
 .then(memeInfo =>{
-    scroller(memeInfo)
+    populatePage(memeInfo);
 })
 
-function scroller(memeInfo){
-    navItem.src = memeInfo.data.memes["1"].url
-        memeFeed.append(navItem)
-    }
-    
-// function addMeme(){
-//     let eachMeme = document.createElement("img")
-//     eachMeme.src = //url here
-//     memeFeed.append(eachMeme)
-// }
+// function scroller(memeInfo ,i){
+//     navItem.src = memeInfo.data.memes[0].url
+//         memeFeed.append(navItem)
 
-// addMeme()
+function populatePage(memeInfo){
+    for (let i = 0; i < memeInfo.data.memes.length; i++) {
+        console.log(memeInfo.data.memes[i]);
+        let img = document.createElement('img')
+        img.src = memeInfo.data.memes[i].url
+        navItem.appendChild(img)
+        memeFeed.append(navItem)
+
+    }
+} 
