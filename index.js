e71b3f4076bb3278cec91c5b999d5a2745c67e1d
Nicolas Telep <ndtelep@gmail.com>
2022-08-24 10:48:28 -0600

    rudimentary dark mode included

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let memeFeed = document.getElementById("meme-list")
 let navItem = document.createElement('h5')
-let i = 0
+let addingButton = document.querySelectorAll('h5')
+let allButtons = document.querySelectorAll('button')
+let toggleButton = document.getElementById("mode-btn")
 fetch("https://api.imgflip.com/get_memes")
 .then(response => response.json())
 .then(memeInfo =>{
     populatePage(memeInfo);
 })
-
-// function scroller(memeInfo ,i){
-//     navItem.src = memeInfo.data.memes[0].url
-//         memeFeed.append(navItem)
 
 function populatePage(memeInfo){
     for (let i = 0; i < memeInfo.data.memes.length; i++) {
@@ -20,4 +18,23 @@ function populatePage(memeInfo){
         memeFeed.append(navItem)
 
     }
-} 
+}
+
+// toggleButton.addEventListener('click', function changeButtonColor(){
+//     allButtons.forEach (); {
+//         style.background = 'black'}
+//     allButtons.style.color = "#FFFF00"
+// });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
